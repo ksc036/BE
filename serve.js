85562e6a379c90d638e0300df1ff512c20fccd75
ksc <ksc036@gmail.com>
2023-07-12 14:37:22 +0900

    #2

--- serve.js
+++ serve.js
@@ -1,11 +1,33 @@
 import express from "express";
+import http from "http";
 import cors from 'cors';
+import WebSocket,{WebSocketServer} from "ws";
 
 
 const app = express();
 
 console.log("hello");
 app.use(cors({ origin: 'http://localhost:8080'}));
-app.get("/",(req,res) => res.send("home"));
+app.get("/",(req,res) => res.send("change"));
  
-app.listen(9000);
+const handleListen = () => console.log("Listening on 9000");
+
+const server = http.createServer(app);
+
+
+const wss = new WebSocketServer({server});
+
+wss.on("connection",(socket) =>{
+console.log("Connected to Browser");
+   socket.send("hello!!!");
+
+   socket.on("close", ()=>{
+    console.log("연결하나 끊어짐");
+   })
+
+   socket.on("message",(data)=>{
+    console.log(data.toString());
+   })
+});
+
+server.listen(9000, handleListen);
