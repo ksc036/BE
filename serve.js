3238ddba2a66a62c4e2c1e7f7bf3fdbd2d9c3e5c
ksc <ksc036@gmail.com>
2023-07-27 07:01:13 +0900

    wss

--- serve.js
+++ serve.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 
 const app = express();
-const uri = "http://i9a701.p.ssafy.io/";
+const uri = "wss://i9a701.p.ssafy.io/";
 const port = 4060;
 //setting cors 
 app.use(cors({
