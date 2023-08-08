04e60e62db5eba5dbbaebf66a622e9908362f022
ksc <ksc036@gmail.com>
2023-08-08 16:25:52 +0900

    localhost3060 날림

--- serve.js
+++ serve.js
@@ -4,18 +4,18 @@ const cors = require("cors");
 
 
 const app = express();
-const uri = "wss://i9a701.p.ssafy.io/";
-const uri2= "http://localhost:3060";
+// const uri = "wss://i9a701.p.ssafy.io/";
+const uri= "http://localhost:3060/";
 const port = 4060;
 //setting cors 
 app.use(cors({
-    origin: [uri,uri2],
+    origin: uri,
     credentials: true
   }));
 
 app.all('/*', function(req, res, next) {
     console.log(res);
-    res.header("Access-Control-Allow-Origin", uri2);
+    res.header("Access-Control-Allow-Origin", uri);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
@@ -25,7 +25,7 @@ app.get("/",(req,res) =>{
 const httpServer = http.createServer(app);
 var io = require('socket.io')(httpServer, {
     cors: {
-        origin: [uri,uri2],
+        origin: uri,
         transports: ['websocket', 'polling'],
         credentials: true
     },
@@ -33,13 +33,13 @@ var io = require('socket.io')(httpServer, {
     transportOptions: {
         polling: {
             extraHeaders: { // Polling transport의 헤더 설정
-                "Access-Control-Allow-Origin": uri2,
+                "Access-Control-Allow-Origin": uri,
                 "Access-Control-Allow-Headers": "X-Requested-With"// 예: 토큰 기반 인증 헤더 추가
               }
         },
         websocket: {
           extraHeaders: { // WebSocket transport의 헤더 설정
-            "Access-Control-Allow-Origin": uri2,
+            "Access-Control-Allow-Origin": uri,
             "Access-Control-Allow-Headers": "X-Requested-With"// 예: 토큰 기반 인증 헤더 추가
           }
         }
